9d52ad5b4291648e94b8302b04f2f7b3c40d8171
Tadeu Mendes <tadeu.mendes@gmail.com>
2023-11-04 16:37:45 -0300

    go

---
+++ funcoes.js
@@ -0,0 +1,42 @@
+// function miar() {
+//     let texto = "miau";
+//     alert(texto);
+// }
+// miar( )
+
+// function nome(){
+//     let texto = "Tadeu";
+//     alert(texto);
+// }
+// nome()
+
+// function qualseunome() {
+//     let nome = prompt("Qual seu nome?")
+//     alert(nome)
+// }
+// qualseunome()
+
+// function pi(){
+//     return 3.14;
+
+
+// }
+// alert(pi())
+
+// function quadrado(lado) {
+//     return Lado * Lado;
+
+// }
+// Let retorno = quadrado(2);
+// alert(retorno)
+
+// function mult(b){
+//     return b * 5;
+    
+// }
+// alert(mult(22)) 
+
+function soma(numero1, numero2){
+    return numero1 + numero2;
+}
+alert(soma(4,5))
